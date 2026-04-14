e0f3f7eda1281c9de85cfd72646af2a118b39d36
ALivingOrange <eastsideethan@icloud.com>
2026-04-14 12:19:59 -0700

    Add controller which accepts JointStateMessage

--- Assets/Members/Ethan/RandomJointStateProvider.cs
+++ Assets/Members/Ethan/RandomJointStateProvider.cs
@@ -1,69 +1,91 @@
 using System;
-using System.Collections;
-using System.Linq;
 using UnityEngine;
 
 /// <summary>
-/// Drives random joint targets into a URDF robot's ArticulationBody chain.
-/// Attach to the same GameObject that has the URDF Controller component.
+/// Mock provider emitting smooth, physically consistent joint states.
+/// Each joint follows a sinusoidal trajectory with randomized parameters,
+/// producing continuous position/velocity pairs that mimic real servo motion.
 /// </summary>
 public class RandomJointStateProvider : MonoBehaviour, IJointStateProvider
 {
-    public GameObject machine;
+	[Tooltip("Emit interval in seconds")]
+	public float publishRate = 0.1f;
 
-    [SerializeField] private float publishRateHz = 10f;
-    [SerializeField] private float minPosition = -Mathf.PI;
-    [SerializeField] private float maxPosition = Mathf.PI;
+	[Tooltip("Position amplitude bounds in radians")]
+	public float minAmplitude = 0.3f;
+	public float maxAmplitude = 1.57f;
 
-    public event Action<JointStateMessage> OnJointStateReceived;
+	[Tooltip("Oscillation frequency bounds in Hz")]
+	public float minFrequency = 0.1f;
+	public float maxFrequency = 0.5f;
 
-    private ArticulationBody[] _joints;
-    private JointStateMessage _latest;
+	public event Action<JointStateMessage> OnJointStateReceived;
 
-    public JointStateMessage GetLatestJointState() => _latest;
+	private string[] jointNames;
+	private float[] amplitudes;
+	private float[] frequencies;
+	private float[] phases;
+	private float timer;
+	private JointStateMessage latestMessage;
 
-    private void Start()
-    {
-        Debug.Log("starts");
-        // Filter to only movable joints (skip the fixed root body)
-        _joints = machine.GetComponentsInChildren<ArticulationBody>()
-            .Where(ab => ab.jointType != ArticulationJointType.FixedJoint)
-            .ToArray();
-        Debug.Log(_joints);
-        StartCoroutine(PublishLoop());
-    }
+	void Start()
+	{
+		var bodies = GetComponentsInChildren<ArticulationBody>();
+		var names = new System.Collections.Generic.List<string>();
 
-    private IEnumerator PublishLoop()
-    {
-        var interval = new WaitForSeconds(1f / publishRateHz);
+		foreach (var body in bodies)
+		{
+			if (body.jointType != ArticulationJointType.FixedJoint)
+				names.Add(body.name);
+		}
 
-        while (true)
-        {
-            _latest = BuildMessage();
-            OnJointStateReceived?.Invoke(_latest);
-            yield return interval;
-        }
-    }
+		jointNames = names.ToArray();
+		int count = jointNames.Length;
 
-    private JointStateMessage BuildMessage()
-    {
-        int count = _joints.Length;
-        var msg = new JointStateMessage
-        {
-            name = new string[count],
-            position = new float[count],
-            velocity = new float[count],
-            effort = new float[count]
-        };
+		amplitudes = new float[count];
+		frequencies = new float[count];
+		phases = new float[count];
 
-        for (int i = 0; i < count; i++)
-        {
-            msg.name[i] = _joints[i].name;
-            msg.position[i] = UnityEngine.Random.Range(minPosition, maxPosition);
-            msg.velocity[i] = UnityEngine.Random.Range(-1f, 1f);
-            msg.effort[i] = 0f;
-        }
+		for (int i = 0; i < count; i++)
+		{
+			amplitudes[i] = UnityEngine.Random.Range(minAmplitude, maxAmplitude);
+			frequencies[i] = UnityEngine.Random.Range(minFrequency, maxFrequency);
+			phases[i] = UnityEngine.Random.Range(0f, Mathf.PI * 2f);
+		}
+	}
 
-        return msg;
-    }
+	void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer < publishRate) return;
+		timer -= publishRate;
+
+		float t = Time.time;
+		int count = jointNames.Length;
+
+		var msg = new JointStateMessage
+		{
+			name = jointNames,
+			position = new float[count],
+			velocity = new float[count],
+			effort = new float[count]
+		};
+
+		for (int i = 0; i < count; i++)
+		{
+			float w = 2f * Mathf.PI * frequencies[i];
+			float theta = w * t + phases[i];
+
+			// position = A * sin(wt + phi)
+			msg.position[i] = amplitudes[i] * Mathf.Sin(theta);
+
+			// velocity = d/dt position = A * w * cos(wt + phi)
+			msg.velocity[i] = amplitudes[i] * w * Mathf.Cos(theta);
+		}
+
+		latestMessage = msg;
+		OnJointStateReceived?.Invoke(msg);
+	}
+
+	public JointStateMessage GetLatestJointState() => latestMessage;
 }
