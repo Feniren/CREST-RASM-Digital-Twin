6c1fec9574dda42b54a130185d84f62568adb505
Feniren <sanders.aidan.c.ss@gmail.com>
2026-04-16 19:06:24 -0700

    Finished basic XR integration

--- Assets/Scripts/Player Scripts/Entity_XR_Hand.cs
+++ Assets/Scripts/Player Scripts/Entity_XR_Hand.cs
@@ -1,18 +1,34 @@
+using System;
 using UnityEngine;
 
 public class Entity_XR_Hand : MonoBehaviour{
-    public Entity_Player PlayerReference;
+	GameObject ItemReference;
+	
+	public Entity_XR_Hand(){
+	}
+	
+	public void GrabStart(Entity_Player PlayerReference, GameObject Item){
+		ItemReference = Item;
 
-    public Entity_XR_Hand(){
-    }
+		if (ItemReference.GetComponent<Item_Parent>().Pickup){
+			ItemReference.transform.SetParent(gameObject.transform);
 
-    public void OnTriggerEnter(Collider Other){
-        Debug.Log(Other.gameObject.name);
+			ItemReference.GetComponent<Rigidbody>().isKinematic = true;
 
-        if (Other.gameObject.GetComponent<Item_Parent>()){
-            Debug.Log("Is an item");
+			GetComponent<Collider>().enabled = false;
+		}
+		else{
+			ItemReference.GetComponent<Item_Parent>().AlternateInteract(PlayerReference);
+		}
+	}
 
-            Other.gameObject.GetComponent<Item_Parent>().AlternateInteract(PlayerReference);
-        }
-    }
+	public void GrabEnd(){
+		if (!GetComponent<Collider>().enabled){
+			ItemReference.transform.SetParent(null, true);
+			
+			ItemReference.GetComponent<Rigidbody>().isKinematic = false;
+
+			GetComponent<Collider>().enabled = true;
+		}
+	}
 }
