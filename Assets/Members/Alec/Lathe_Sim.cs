7a51b4664acf525cd81b0215fb27e274467c1bd9
Alec Liu <sammiliu71@gmail.com>
2026-05-27 14:49:28 -0700

    Implemented facing and grooving operations, both have been tested and output correctly.

--- Assets/Members/Alec/Lathe_Sim.cs
+++ Assets/Members/Alec/Lathe_Sim.cs
@@ -15,7 +15,9 @@ public class Lathe_Sim : MonoBehaviour
     
 
     void Start() {
-        Drilling(3, .5f, 1);
+        // Drilling(3, .5f);
+        // Turning();
+        Grooving();
         // test();
     }
     // CSG quick tutorial guide
@@ -38,6 +40,73 @@ public class Lathe_Sim : MonoBehaviour
         composite.AddComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
     }
 
+    public void Drilling(float diameter = 1f, float depth = 1f, float positionOffset = 0f) // depth = y, diameter = x,z
+    {
+        /*
+            1. spawn tool prefab + set up tol position, size, etc
+            2. perform subtraction
+            3. clone component details from old stock
+            4. copy onto new + add new meshes
+        */
+        GameObject toolInstance = ToolSetup(diameter, depth, positionOffset);
+
+        Model result = CSG.Subtract(stockObj, toolInstance/*.transform.GetChild(0).gameObject*/);
+
+        GameObject composite = Cloning(stockObj);
+        
+        MeshCopy(result, composite);
+
+        composite.name = stockObj.name;
+
+        Destroy(toolInstance);
+        Destroy(stockObj);
+    }
+
+    public void Facing(float depth = 1f)
+    {
+        float stockDiameter = stockObj.transform.lossyScale.x;
+        Drilling(stockDiameter, depth);
+    }
+
+    public void Turning(float diameter = 0.5f, float depth = 0.5f, float positionOffset = 0f) {
+        /*
+            1. spawn two tool prefabs: one for subtraction and other for union
+                a. subtraction tool prefab diameter is to be bigger than stock obj's diameter
+                b. union will take in actual diameter + slack
+            2. subtract using given depth with subtraction prefab, 
+            3. output into a new gameObject (temp output)
+            4. union new gameobject with union tool prefab
+            5. output into a 2nd new gameObject, set this one as new stockObj
+            6. delete all prefabs + temp output
+        */
+        // float slackDiameter = stockObj.l
+
+        float subDiameter = stockObj.transform.lossyScale.x+1;
+        GameObject unionTool = ToolSetup(diameter, depth, positionOffset), subTool = ToolSetup(subDiameter, depth, positionOffset);
+
+        Model subResult = CSG.Subtract(stockObj, subTool);
+
+        GameObject subComposite = new GameObject("sub-composite"), composite;
+        MeshCopy(subResult, subComposite);
+
+        Model result = CSG.Union(subComposite, unionTool);
+        DestroyImmediate(subComposite);
+        
+        composite = Cloning(stockObj);
+
+        MeshCopy(result, composite);
+        composite.name = stockObj.name;
+
+        Destroy(unionTool);
+        Destroy(subTool);
+        Destroy(stockObj);
+    }
+
+    public void Grooving(float diameter = .5f, float depth = .5f, float positionOffset = 1f)
+    {
+        Turning(diameter, depth, positionOffset);
+    }
+
     private GameObject ToolSetup(float diameter = 1f, float depth = 1f, float positionOffset = 0f)
     {
         // if dimensions don't make sense, throw an error message/exception
@@ -55,9 +124,10 @@ public class Lathe_Sim : MonoBehaviour
         return tool;
     }
 
-    public void Cloning(GameObject original, GameObject clone) // work in progress
+    public GameObject Cloning(GameObject original) // work in progress
     {
-        List<string> exclusions = new List<string>{"MeshFilter", "MeshRenderer"};
+        GameObject clone = new GameObject("stock clone");
+        List<string> exclusions = new List<string>{"MeshFilter", "MeshRenderer", "CapsuleCollider", "Transform"};
         Component[] components = original.GetComponents(typeof(Component));
 
         foreach (Component component in components)
@@ -72,48 +142,13 @@ public class Lathe_Sim : MonoBehaviour
                 clone.CopyComponent(component);
             }
         }
+        return clone;
     }
-
-    public void Drilling(float diameter = 1f, float depth = 1f, float positionOffset = 0f) // depth = y, diameter = x,z
+    
+    private void MeshCopy(Model result, GameObject composite)
     {
-        /*
-            1. spawn tool prefab
-            2. set tool size
-            3. set tool position to designated spot (main.y - depth)
-            4. perform subtraction  
-        */
-        GameObject toolInstance = ToolSetup(diameter, depth, positionOffset);
-
-        // Model result = CSG.Subtract(stockObj, toolInstance.transform.GetChild(0).gameObject);
-
-        // GameObject composite = new GameObject();
-        // // GameObject composite = Cloning(stockObj);
-
-        
-        // MeshCopy(result, composite);
-
-        // // what's supposed to happen after everything works properly:
-        // string name = stockObj.name;
-
-        // Destroy(toolInstance);
-        // Destroy(stockObj);
-
-        // stockObj = composite;
-        // stockObj.name = name;
-    }
-
-    public void Turning(float diameter = 0.5f, float depth = 0.5f) {
-        /*
-            1. spawn two tool prefabs: one for subtraction and other for union
-                a. subtraction tool prefab diameter is to be bigger than stock obj's diameter
-                b. union will take in actual diameter + slack
-            2. subtract using given depth with subtraction prefab, 
-            3. output into a new gameObject (temp output)
-            4. union new gameobject with union tool prefab
-            5. output into a 2nd new gameObject, set this one as new stockObj
-            6. delete all prefabs + temp output
-        */
-        // float slackDiameter = stockObj.l
-        GameObject unionTool = Instantiate(toolPrefab), subTool = Instantiate(toolPrefab);
+        composite.AddComponent<MeshFilter>().sharedMesh = result.mesh;
+        composite.AddComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
+        composite.AddComponent<MeshCollider>().sharedMesh = result.mesh; // check in to see if this is what we want
     }
 }
